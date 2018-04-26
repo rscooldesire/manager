83199c99e9fcda5a24d7006eb66ab60925a9e7a4
Ranjit Singh <rscooldesire@gmail.com>
2018-04-26 19:24:11 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
+import reducers from './reducers';
+import Router from './Router';
+
+class App extends Component {
+    componentWillMount() {
+        const config = {
+            apiKey: 'AIzaSyCyNhmZaOQ2pwGtf2eaGbwH7mK-PhdXUYs',
+            authDomain: 'manager-4fc70.firebaseapp.com',
+            databaseURL: 'https://manager-4fc70.firebaseio.com',
+            projectId: 'manager-4fc70',
+            storageBucket: 'manager-4fc70.appspot.com',
+            messagingSenderId: '878172761573'
+          };
+          firebase.initializeApp(config);
+    }
+    render() {
+        const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+        return (
+            <Provider store={store}>
+                <Router />
+            </Provider>
+        );
+    }
+}
+
+export default App;
